a3c1c524890968f9a25b9a1dd78ee86aef286aa5
Kajal lathiya <kajalbodarya77@gmail.com>
2023-01-18 00:04:40 +0530

    add blogpost api

--- src/components/blog/blog-list/BlogList.jsx
+++ src/components/blog/blog-list/BlogList.jsx
@@ -1,16 +1,30 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Col, Row } from "react-bootstrap";
 import posts from "../../../data/posts.json";
 import BlogItem from "../blog-item/BlogItem";
 
+const BASE_URL = process.env.REACT_APP_BASE_URL;
+
 const BlogList = (props) => {
+  useEffect(() => {
+    fetchStriveBlogposts();
+  }, []);
+  const fetchStriveBlogposts = async () => {
+    try {
+      const response = await fetch(`${BASE_URL}/mongoBlogPosts`);
+      const result = await response.json();
+      console.log("RESULT:-", result);
+    } catch (error) {
+      console.log(error);
+    }
+  };
   return (
     <Row>
       {posts.map((post) => (
         <Col
           md={4}
           style={{
-            marginBottom: 50,
+            marginBottom: 50
           }}
         >
           <BlogItem key={post.title} {...post} />
